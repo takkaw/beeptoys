d3e0016c1d06952e4a8cd6e2948cd6317d959e9b
takkaw <takkaw@gmail.com>
2008-06-22 09:57:00 +0900

    debug unless narray library

--- lib/narray_helper.rb
+++ lib/narray_helper.rb
@@ -1,38 +1,39 @@
 try_require 'narray'
 
-class Array
-  if defined? NArray
+if defined? NArray
+  class Array
     def to_na
       NArray.to_na(self)
     end
-  else
+
+    def flatten_
+      self.flatten
+    end
+  end
+
+  class NArray
+    def to_na
+      self
+    end
+    def reverse
+      self.to_a.reverse.to_na
+    end
+    def flatten_
+      self.empty? ? self : self.flatten
+    end
+    def compact
+      self.to_a.compact
+    end
+    def compact!
+      self.to_a.replace(self.compact).to_na
+    end
+  end
+else
+  class Array
     def to_na
       self
     end
   end
-
-  def flatten_
-    self.flatten
-  end
-end
-
-class NArray
-  def to_na
-    self
-  end
-  def reverse
-    self.to_a.reverse.to_na
-  end
-  def flatten_
-    self.empty? ? self : self.flatten
-  end
-  def compact
-    self.to_a.compact
-  end
-  def compact!
-    self.to_a.replace(self.compact).to_na
-  end
-
 end
 
 def empty
