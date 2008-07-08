8cd1f2fd6f0824158b0741bca601468825cf208b
takkaw <takkaw@gmail.com>
2008-07-09 07:28:47 +0900

    refactoring collection module

--- lib/irbrc.rb
+++ lib/irbrc.rb
@@ -1,16 +1,10 @@
 # Collection module for auto collection in irb.
 module Collection
   wave_dir = BeepConfig[:wave_dir] + '/'
-  s = Dir[wave_dir + '*.wav'].collect { |w|
-    w.gsub(File.extname(w),'').gsub(wave_dir,'')
-  }
-  s.each { |collect|
-    collect = '_' + collect if /[0-9]/ =~ collect[0..0]
-    module_eval %-
-      def #{collect}
-        :#{collect}
-      end
-    -
+  Dir[wave_dir + '*.wav'].collect { |w|
+    w.gsub!(File.extname(w),'').gsub!(wave_dir,'')
+    w = '_' + w if /[0-9]/ =~ w[0..0]
+    w.to_sym
   }
 end
 
