1a52d6d89c60dcabd5280de21d94942b78a17dc0
takkaw <takkaw@gmail.com>
2008-07-09 21:27:54 +0900

    refactoring filename collector

--- lib/irbrc.rb
+++ lib/irbrc.rb
@@ -1,10 +1,7 @@
-# Collection module for auto collection in irb.
-module Collection
-  wave_dir = BeepConfig[:wave_dir] + '/'
-  Dir[wave_dir + '*.wav'].collect { |w|
-    w.gsub!(File.extname(w),'').gsub!(wave_dir,'')
-    w = '_' + w if /[0-9]/ =~ w[0..0]
-    w.to_sym
-  }
-end
-
+# To auto collect in irb.
+wave_dir = BeepConfig[:wave_dir] + '/'
+Dir[wave_dir + '*.wav'].collect { |w|
+  w.gsub!(File.extname(w),'').gsub!(wave_dir,'')
+  w = '_' + w if /[0-9]/ =~ w[0..0]
+  w.to_sym
+}
