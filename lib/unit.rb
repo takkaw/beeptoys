29e670ff21d53143047f702a29f46b738a884afd
takkaw <takkaw@gmail.com>
2008-10-21 19:02:24 +0900

    add time unit

--- lib/unit.rb
+++ lib/unit.rb
@@ -10,9 +10,14 @@ class Numeric
   }
 
   unit.each { |unit,ret|
-    eval %Q( def #{unit} ;  { :#{ret} => self * 1000**0} ; end )
+    eval %Q( def  #{unit} ; { :#{ret} => self * 1000**0} ; end )
     eval %Q( def k#{unit} ; { :#{ret} => self * 1000**1} ; end )
     eval %Q( def m#{unit} ; { :#{ret} => self * 1000**2} ; end )
   }
+
+  def sec ; { self * 60 ** 0 ,lambda { @sample_rate } } ; end 
+  def min ; { self * 60 ** 1 ,lambda { @sample_rate } } ; end 
+  def hour; { self * 60 ** 2 ,lambda { @sample_rate } } ; end 
+
 end
 
